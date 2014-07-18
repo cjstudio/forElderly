415645537aa8976ecbb9b946e135ffb40e73b0b2
cjstudio <wangcj1214@gmail.com>
2014-07-18 13:21:39 +0800

    SignUp_Update

--- FC/Account/SignUpScript.aspx.cs
+++ FC/Account/SignUpScript.aspx.cs
@@ -9,9 +9,41 @@ namespace FC.Account
 {
     public partial class SignUpScript : System.Web.UI.Page
     {
+        struct User {
+            public string id,name,password32,email;
+            public int type;
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            User u_input = new User();
+            try
+            {
+                u_input.name  = HttpContext.Current.Request["username"].ToString();
+                passwd = HttpContext.Current.Request["password"];
+                member = HttpContext.Current.Request["member"];
+                if (!isLawfulInput(username) || !isLawfulInput(passwd))
+                {
+                    Response.Write("{ \"status\" : \"Error\", \"msg\" : \"用户名或密码中存在敏感字符\"}");
+                    Response.End();
+                }
+                if (username == "" || passwd == "")
+                {
+                    Response.Write("{ \"status\" : \"Error\", \"msg\" : \"用户名或密码不能为空\"}");
+                    Response.End();
+                }
+                Response.Clear();
+            }
+            catch (Exception)
+            {
+                Response.Write("{ \"status\" : \"Other\", \"msg\" : \"Json 数据传输错误\"}");
+                Response.End();
+                return;
+            }
+            if (identificaUser())
+            {
+                ;
+            }
+            Response.End();
         }
     }
 }
